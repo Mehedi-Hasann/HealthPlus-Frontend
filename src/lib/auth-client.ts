1b19cb91e7f7dd90263fa1de8f9c2ec40bbe2556
Mehedi Hasan <u1904067@student.cuet.ac.bd>
2026-05-05 01:00:09 +0600

    update auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,17 @@
 import { createAuthClient } from "better-auth/react"
+
+const getBaseURL = () => {
+    if (typeof window !== "undefined") return window.location.origin;
+    if (process.env.NEXT_PUBLIC_APP_URL) return process.env.NEXT_PUBLIC_APP_URL;
+    if (process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+    return process.env.FRONTEND_URL;
+}
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: typeof window !== "undefined" ? window.location.origin : "",
+    baseURL: getBaseURL(),
     basePath : "/api/auth",
     fetchOptions : {
         credentials : "include",
     }
-    // baseURL: "http://localhost:3000/"
 })
