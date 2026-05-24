2ecc1909b2c117c41a16a44e54ba8338419e3a15
Mehedi Hasan <u1904067@student.cuet.ac.bd>
2026-05-24 15:42:41 +0600

    fix issues

--- src/components/modules/authentication/login-form.tsx
+++ src/components/modules/authentication/login-form.tsx
@@ -46,12 +46,14 @@ export function LoginForm({ ...props }: React.ComponentProps<typeof Card>) {
 
         Cookies.set("accessToken", accessToken, {
           expires: 7, // 7 days
+          path: "/",
           secure: process.env.NODE_ENV === "production",
           sameSite: "lax",
         });
 
         Cookies.set("refreshToken", refreshToken, {
           expires: 7,
+          path: "/",
           secure: process.env.NODE_ENV === "production",
           sameSite: "lax",
         });
